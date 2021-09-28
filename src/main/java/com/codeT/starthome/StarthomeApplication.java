f986f00c12bcf742e7853f4e5332bd63d35836da
YOU <youjin381@gmail.com>
2021-09-28 19:58:59 +0900

    init

---
+++ src/main/java/com/codeT/starthome/StarthomeApplication.java
@@ -0,0 +1,13 @@
+package com.codeT.starthome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarthomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarthomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeT/starthome/StarthomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeT.starthome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarthomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
