e47cccc938f4fa15006b76365f5b04ea3ed6c35f
YOU <youjin381@gmail.com>
2021-09-28 20:51:45 +0900

    init

--- src/main/java/com/codeT/starthome/controller/StarthomeController.java
+++ src/main/java/com/codeT/starthome/controller/StarthomeController.java
@@ -1,4 +1,13 @@
 package com.codeT.starthome.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StarthomeController {
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
 }
